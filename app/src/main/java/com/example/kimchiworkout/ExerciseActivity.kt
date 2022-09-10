c38f872c677d035e31e8e56fc09713e255cb3e45
Yiro Yi <yiroyi@live.com>
2022-09-10 23:57:18 +0900

    Added counter

--- app/src/main/java/com/example/kimchiworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/kimchiworkout/ExerciseActivity.kt
@@ -2,10 +2,16 @@ package com.example.kimchiworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.example.kimchiworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
   private var binding:ActivityExerciseBinding? = null
+
+  private var restTimer: CountDownTimer? = null
+  private var restProgress = 0
+
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -22,5 +28,48 @@ class ExerciseActivity : AppCompatActivity() {
       onBackPressed()
     }
 
+    setupRestView()
+
+  }
+
+  private fun setupRestView() {
+    if(restTimer != null) {
+      restTimer?.cancel()
+      restProgress = 0
+    }
+
+    setRestProgressBar()
+  }
+
+  private fun setRestProgressBar() {
+    binding?.progressBar?.progress =restProgress
+
+    restTimer = object: CountDownTimer(10000, 1000) {
+      override fun onTick(p0: Long) {
+        restProgress++
+        binding?.progressBar?.progress = 10 - restProgress
+        binding?.tvTimer?.text = (10 - restProgress).toString()
+      }
+
+      override fun onFinish() {
+        Toast.makeText(
+          this@ExerciseActivity,
+          "Here now we will start the exercise.",
+          Toast.LENGTH_LONG
+        ).show()
+      }
+
+    }.start()
+  }
+
+  override fun onDestroy() { // this on destroy must be added when we use binding
+    // This happens when the view is destroyed or we move to another one
+    super.onDestroy()
+
+    if(restTimer != null) {
+      restTimer?.cancel()
+      restProgress = 0
+    }
+    binding = null
   }
 }
