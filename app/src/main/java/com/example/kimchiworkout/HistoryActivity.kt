98dd3e0e5cd68dd219a70cba251ef82bea688566
Yiro Yi <yiroyi@live.com>
2022-09-17 15:49:43 +0900

    Added history layout

--- app/src/main/java/com/example/kimchiworkout/HistoryActivity.kt
+++ app/src/main/java/com/example/kimchiworkout/HistoryActivity.kt
@@ -2,7 +2,12 @@ package com.example.kimchiworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kimchiworkout.databinding.ActivityHistoryBinding
+import kotlinx.coroutines.launch
 
 class HistoryActivity : AppCompatActivity() {
   private var binding: ActivityHistoryBinding? = null
@@ -21,5 +26,45 @@ class HistoryActivity : AppCompatActivity() {
       binding?.toolbarHistoryActivity?.setNavigationOnClickListener {
         onBackPressed()
       }
+
+    val historyDao = (application as WorkOutApp).db.historyDao()
+    getAllCompletedDates(historyDao)
+  }
+
+  private fun getAllCompletedDates(historyDao: HistoryDao){
+    Log.e("PROCESS YIRO",  "WATCH")
+    lifecycleScope.launch {
+      historyDao.fetchAllDates().collect { allCompletedDatesList ->
+        if(allCompletedDatesList.isNotEmpty()) {
+          binding?.tvHistory?.visibility = View.VISIBLE
+          binding?.rvHistory?.visibility = View.VISIBLE
+
+          binding?.tvNoDataAvailable?.visibility = View.INVISIBLE
+
+          binding?.rvHistory?.layoutManager = LinearLayoutManager(this@HistoryActivity)
+
+          val dates = ArrayList<String>()
+
+          for(date in allCompletedDatesList) {
+            dates.add(date.date)
+          }
+
+          val historyAdapter = HistoryAdapter(dates)
+
+          binding?.rvHistory?.adapter = historyAdapter
+
+        } else {
+          binding?.tvHistory?.visibility = View.GONE
+          binding?.rvHistory?.visibility = View.GONE
+
+          binding?.tvNoDataAvailable?.visibility = View.VISIBLE
+        }
+      }
+    }
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    binding = null
   }
 }
